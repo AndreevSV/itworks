f8d39d6bbba41d869d0e1c373e6d709d42ac8078
AndreevSV <andreevsv@gmail.com>
2024-03-11 09:31:32 +0200

    Array exercise

--- exercises/week-3/javascript_intro/main.js
+++ exercises/week-3/javascript_intro/main.js
@@ -1,26 +1,17 @@
-let boughtTesla = true;
-const yearOfTeslaPurchase = 2014;
-let isUSCitizen = true;
-let currentYear = 2018;
+const numbers = [1,2,3,4,5,6,7,8,9,10];
 
-function checkAgeOfTesla(yearOfTeslaPurchase, currentYear) {
-    if (currentYear - yearOfTeslaPurchase >= 4) {
-        return "Would you like an upgrade for you Tesla?";
-    } else {
-        return "Are you satisfied with your Tesla?";
-    }
-}
+numbers.splice(1, 2);
 
-function checkForBoughtAndCitizenship(boughtTesla, isUSCitizen){
-    if (boughtTesla == true && isUSCitizen == true) {
-        return checkAgeOfTesla(yearOfTeslaPurchase, currentYear);
-    } else if (boughtTesla == true && isUSCitizen == false) {
-        return "Would you like to move to the US?";
-    } else if (boughtTesla == false) {
-        return "Would you like to buy a Tesla?";
-    }
-}
+console.log(numbers);
 
-console.log(checkForBoughtAndCitizenship(boughtTesla, isUSCitizen));
+numbers.splice(3, 1, 1);
 
+console.log(numbers);
 
+numbers.splice(-4);
+
+console.log(numbers);
+
+numbers.splice(0, 0, 0);
+
+console.log(numbers);
