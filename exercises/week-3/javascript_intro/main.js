b13408ab2841bec653290c08b6d5f97ede178201
AndreevSV <andreevsv@gmail.com>
2024-03-11 08:10:11 +0200

    AI Tesla worker

--- exercises/week-3/javascript_intro/main.js
+++ exercises/week-3/javascript_intro/main.js
@@ -1,1 +1,26 @@
-console.log("Hello, world!")
+let boughtTesla = true;
+const yearOfTeslaPurchase = 2014;
+let isUSCitizen = true;
+let currentYear = 2018;
+
+function checkAgeOfTesla(yearOfTeslaPurchase, currentYear) {
+    if (currentYear - yearOfTeslaPurchase >= 4) {
+        return "Would you like an upgrade for you Tesla?";
+    } else {
+        return "Are you satisfied with your Tesla?";
+    }
+}
+
+function checkForBoughtAndCitizenship(boughtTesla, isUSCitizen){
+    if (boughtTesla == true && isUSCitizen == true) {
+        return checkAgeOfTesla(yearOfTeslaPurchase, currentYear);
+    } else if (boughtTesla == true && isUSCitizen == false) {
+        return "Would you like to move to the US?";
+    } else if (boughtTesla == false) {
+        return "Would you like to buy a Tesla?";
+    }
+}
+
+console.log(checkForBoughtAndCitizenship(boughtTesla, isUSCitizen));
+
+
