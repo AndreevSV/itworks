c949719750fa40248163b79393e2115ea207740e
AndreevSV <andreevsv@gmail.com>
2024-03-12 13:52:11 +0200

    Exercise 1 done

---
+++ exercises/week-3/javascript_functions/app.js
@@ -0,0 +1,19 @@
+// Exercise 1
+// Write a function called isEven that accepts a number as a parameter and checks 
+// if the number is even or not. Return true if it is even or false if it is odd. 
+// Don’t Google this one ;) Use modulo %
+
+function isEven(number) {
+    if(number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isEven(11));
+
+// Exercise 2
+// Write a function that takes in an array of numbers.
+// The function should loop through the numbers and (using the function from Exercise 1) 
+// print out the odd numbers.
