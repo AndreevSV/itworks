a58925e691eaff44105845ebad6c7141149ac855
AndreevSV <andreevsv@gmail.com>
2024-03-12 11:54:48 +0200

    Exercise 4 done

--- exercises/week-3/javascript_intro/app.js
+++ exercises/week-3/javascript_intro/app.js
@@ -1,5 +1,5 @@
 //Loops
-
+/*
 //Exercises
 
 //Exercise 1
@@ -37,3 +37,109 @@ function logPhrases(people) {
 }
 
 logPhrases(people);
+
+
+//Exercise 3
+// ≡ The following exercises are a little more challenging - but this is a good chance to practice your programmer’s mind. ≡
+// Given this array:
+// const posts = [
+//   {id: 1, text: "Love this product"},
+//   {id: 2, text: "This is the worst. DON'T BUY!"},
+//   {id: 3, text: "So glad I found this. Bought four already!"}
+// ]
+// Write some code that removes the post with an id of 2 from the posts array. Can’t have any negative reviews of our product!
+// Obviously, do not do posts.splice(1, 1) - you have to find the post to remove! (Of course, you will use splice - just not hard coded!)
+
+const posts = [
+    {id: 1, text: "Love this product"},
+    {id: 2, text: "This is the worst. DON'T BUY!"},
+    {id: 3, text: "So glad I found this. Bought four already!"}
+  ]
+
+  let postId = 2; // Try to change from 1 till 3
+
+  function removeNegativePost(posts, postId){
+    for (let i = 0; i < posts.length; i++) {
+        if (postId === posts[i].id) {
+            posts.splice(i, 1);
+        }
+    }
+    console.log(posts);
+  }
+
+  removeNegativePost(posts, postId);
+*/
+
+// Exercise 4
+// Given this array:
+// const posts = [
+//   {
+//     id: 1, 
+//     text: "Love this product",
+//     comments: []
+//   },
+//   { 
+//     id: 2, 
+//     text: "This is the worst. DON'T BUY!", 
+//     comments: [
+//                 {id: 1, text: "Idiot has no idea"}, 
+//                 {id: 2, text:"Fool!"}, 
+//                 {id: 3, text: "I agree!"}
+//               ]
+//    },
+//    {
+//     id: 3, 
+//     text: "So glad I found this. Bought four already!",
+//     comments: []
+//    }
+// ]
+// You are given 2 as the ID of a post. Write some code that finds the comment with an ID of 3 (inside of a post with an ID of 2), and remove just that one comment.
+
+const posts2 = [
+    {
+      id: 1, 
+      text: "Love this product",
+      comments: []
+    },
+    { 
+      id: 2, 
+      text: "This is the worst. DON'T BUY!", 
+      comments: [
+                  {id: 1, text: "Idiot has no idea"}, 
+                  {id: 2, text:"Fool!"}, 
+                  {id: 3, text: "I agree!"}
+                ]
+     },
+     {
+      id: 3, 
+      text: "So glad I found this. Bought four already!",
+      comments: []
+     }
+  ]
+
+  let postId2 = 2; 
+  let commentId = 3;
+
+  function removeCommentByPostIdAndCommentId(posts2, postId2, commentId){
+    for (let i = 0; i < posts2.length; i++) {
+        if(postId2 === posts2[i].id) {
+            removeCommentById(posts2[i].comments, commentId);
+            break;
+        }
+    }
+    console.dir(posts2, {depth: null});
+  }
+
+  let comments = [];
+
+  function removeCommentById(comments, commentId) {
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].id === commentId) {
+            comments.splice(i, 1);
+            break;
+        }
+    }      
+  }
+
+    removeCommentByPostIdAndCommentId(posts2, postId2, commentId);
+  
