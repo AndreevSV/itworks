79ed3013ce3ef2af3ec5339628c2a7a2ffb5f3bd
AndreevSV <andreevsv@gmail.com>
2024-03-13 14:17:59 +0200

    Exercises 3 done

---
+++ exercises/week-3/codio-exercises-pdf/ex3.js
@@ -0,0 +1,15 @@
+// JavaScript – functions
+
+// The following exercise contains the following subjects:
+// ● functions
+
+// Instructions
+// Why pay a fortune teller when you can just program your fortune yourself?
+// 1. Write a function named `tellFortune` that takes 4 parameters: jobTitle, location, partnerName, numberOfChildren.
+// Based on this output the function returns a string with this format: ‘You will be a programmer in New York married to Rana with 2 children’
+
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+    console.log("You will be a " + jobTitle + " in " + location + " merried " + "to " + partnerName + " with " + numberOfChildren + " children");    
+}
+
+tellFortune("programmer", "New York", "Rana", 2);
