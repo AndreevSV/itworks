b1d5f99033e8699b8eb78e478bbe820c9d13183a
AndreevSV <andreevsv@gmail.com>
2024-03-20 17:06:53 +0200

    Exercise 6 done

---
+++ exercises/week-4/callbacks_arrow_functions/ex6.js
@@ -0,0 +1,27 @@
+// Exercise 6 - Arrow functions
+
+// Given the following:
+// const determineWeather = (temp) => {
+//   if (temp > 25) {
+//     return "hot";
+//   }
+//   return "cold";
+// };
+// Create a one-line arrow function called commentOnWeather that takes one parameter, temp. 
+// It should call determineWeather and return the concatenation of "It’s " and 
+// determineWeather’s output:
+// commentOnWeather(30); //returns "It's hot"
+// commentOnWeather(22); //returns "It's cold"
+
+
+const determineWeather = (temp) => {
+  if (temp > 25) {
+    return "hot";
+  }
+  return "cold";
+};
+
+const commentOnWeather = (temp) => console.log('It\'s ' + determineWeather(temp));
+
+commentOnWeather(30); //returns "It's hot"
+commentOnWeather(22); //returns "It's cold"
