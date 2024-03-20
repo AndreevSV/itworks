6c2527c12ffeb49c2e9047a6ad2378ca3137e8de
AndreevSV <andreevsv@gmail.com>
2024-03-20 16:47:43 +0200

    Exercise 4 done

---
+++ exercises/week-4/callbacks_arrow_functions/ex4.js
@@ -0,0 +1,7 @@
+// Exercise 4 - Arrow Functions
+// Create an arrow function that receives three parameters and returns their sum - 
+// it should be one line.
+
+let sumFunc = (num1, num2, num3) => console.log(num1 + num2 + num3);
+
+sumFunc(1, 2, 3);
