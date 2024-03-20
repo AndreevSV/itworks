4c2bd0e71b49ea5bd6ec1f0d6b8fc52c2f0abb15
AndreevSV <andreevsv@gmail.com>
2024-03-20 16:53:00 +0200

    Exercise 5 done

---
+++ exercises/week-4/callbacks_arrow_functions/ex5.js
@@ -0,0 +1,13 @@
+// Exercise 5 - Arrow Functions
+
+// Create an arrow function called capitalize that receives any string, and returns it with proper capitalization:
+// capitalize("bOb"); // returns Bob
+// capitalize("TAYLOR"); // returns Taylor
+// capitalize("feliSHIA"); // returns Felishia
+// It can be one line ;)
+
+let capitalize = (str) => console.log(str.toUpperCase());
+
+capitalize("bOb"); // returns Bob
+capitalize("TAYLOR"); // returns Taylor
+capitalize("feliSHIA"); // returns Felishia
